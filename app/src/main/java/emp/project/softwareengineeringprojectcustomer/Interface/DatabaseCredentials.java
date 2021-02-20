d9f88bf846b451838a937c2d4b3dfca2ae31af5b
Aleth Gillian <fernandez.aleth@gmail.com>
2021-02-20 20:07:29 +0800

    change db credential

--- app/src/main/java/emp/project/softwareengineeringprojectcustomer/Interface/DatabaseCredentials.java
+++ app/src/main/java/emp/project/softwareengineeringprojectcustomer/Interface/DatabaseCredentials.java
@@ -2,7 +2,7 @@ package emp.project.softwareengineeringprojectcustomer.Interface;
 
 public interface DatabaseCredentials {
     /**Free Mysql server**/
-    public static final String DB_NAME = "jdbc:mysql://sql12.freemysqlhosting.net:3306/sql12388207";
+    public static final String DB_NAME = "jdbc:mysql://sql12.freemysqlhosting.net:3306/sql12394153";
     public static final String USER = "sql12394153";
     public static final String PASS = "k7TngnBqxC";
 
